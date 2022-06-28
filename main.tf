414ecdbbe5a766718f205684bddfaaaf0d4d9d6e
Andres Lara <andres.felipe.lara@gmail.com>
2022-06-28 10:24:07 +0200

    3C: Add path and method to the APIGateway

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
 module "api_gateway" {
   source = "./modules/api_gateway"
 }
+module "lambda_function" {
+  source = "./modules/lambda_function"
+}
--- modules/api_gateway/rest_api.tf
+++ modules/api_gateway/rest_api.tf
@@ -7,3 +7,19 @@ resource "aws_api_gateway_resource" "rest_api_resource" {
   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
   path_part   = "api"
 }
+
+resource "aws_api_gateway_method" "rest_api_get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.rest_api_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "rest_api_get_method_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.rest_api_resource.id
+  http_method             = aws_api_gateway_method.rest_api_get_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.lambda_function_arn
+}
